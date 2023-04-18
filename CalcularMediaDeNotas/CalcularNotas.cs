94030c72aa9c1d8764a3310c1bf1428893f9618f
devje <devje@HAPLUS>
2023-04-18 14:29:16 -0300

    Add project files.

---
+++ CalcularMediaDeNotas/CalcularNotas.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace CalcularMediaDeNotas
+{
+	internal class CalcularNotas
+	{
+		public int Quantidade;
+		public double[] Notas;
+		
+
+		public void CalcularMedia()
+		{
+			for (int i = 0; i < Quantidade; i++)
+			{
+				Console.WriteLine($"DIGITE A NOTA: {i + 1}");
+				Notas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			}
+
+			double soma = 0;
+
+			for (int i = 0; i < Quantidade; i++)
+			{
+				soma += Notas[i];
+			}
+
+			double media = soma / Quantidade;
+            Console.WriteLine($"SUA MÉDIA: {media.ToString("f1", CultureInfo.InvariantCulture)}");
+        }
+
+
+	}
+}
---
+++ CalcularMediaDeNotas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalcularMediaDeNotas
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			CalcularNotas notas = new CalcularNotas();
+
+			Console.WriteLine("BEM VINDO AO CALCULADOR DE NOTAS!");
+			Console.WriteLine("=================================");
+			Console.Write("QUAL A QUANTIDADE DE NOTAS? ");
+			notas.Quantidade = int.Parse(Console.ReadLine());
+
+			notas.Notas = new double[notas.Quantidade];
+
+			notas.CalcularMedia();
+		}
+	}
+}
